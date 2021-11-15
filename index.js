8f67d34711a7137976ad062b1c85d8a784119d09
Keith Duffy <32164696+DufflogicXD@users.noreply.github.com>
2021-11-15 16:58:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -21,6 +22,9 @@ app.get('/api/users/:id', (req, res) => {
 });
 
 app.post('/api/users', (req, res) => {
+  const { error } = validateUser(req.body);
+  if(error) return res.status(400).send(error.details[0].message);
+  
   const user = {
     id: users.length + 1,
     name: req.body.name
@@ -28,3 +32,22 @@ app.post('/api/users', (req, res) => {
   users.push(user);
   res.send(user);
 });
+
+app.put('/api/users/:id', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if( !user ) return res.status(404).send('The user with the given ID could not be found');
+  
+  const { error } = validateUser(req.body);
+  if(error){
+    res.status(400).send(error.details[0].message);
+  }
+});
+
+function validateUser(user){
+  const schema = {
+    name: Joi.string().min(3).required()
+  }
+  
+  return Joi.validate(user, schema)
+  
+}
