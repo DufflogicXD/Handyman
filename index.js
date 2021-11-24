81c89b1fb1cd240791012f66bc8f7c24b7f788f2
kduffy-imgs <kduffy@imgs.ie>
2021-11-24 14:15:14 +0000

    Added CRUD functions for Service Job

--- index.js
+++ index.js
@@ -34,7 +34,10 @@ app.get('/', (req, res) => {
   res.send('Hello World');  
 });
 
+
+// ############################################################################
 //----------------- CUSTOMER USER RELATED CRUD FUNCTIONS-----------------------
+// ############################################################################
 
 //GET ALL USERS
 app.get('/api/users', (req, res) => {
@@ -152,6 +155,7 @@ function validateCustomerUser(user){
   return schema.validate(user);
   
 }
+
 
 // ############################################################################
 //----------------- BUSINESS USER RELATED CRUD FUNCTIONS-----------------------
@@ -367,6 +371,134 @@ function validateMessage(message){
   });
   
   return schema.validate(message);
+}
+
+
+// ############################################################################
+//----------------- SERVICEJOB RELATED CRUD FUNCTIONS-----------------------
+// ############################################################################
+
+//GET ALL Service Job
+app.get('/api/servicejobs', (req, res) => {
+  const query = "select * from SERVICEJOB";
+  pool.query(query, (error, results) =>{
+    if (!results) res.json({status: "Not found!"});
+    else res.json(results);
+  })
+});
+
+//GET a specific Service Job by ID
+app.get('/api/servicejobs/:id', (req, res) => {
+  const query = "select * from SERVICEJOB WHERE id = ?";
+  pool.query(query, [req.params.id], (error, results) =>{
+    if (!results[0]) res.json({status: "Not found!"});
+    else res.json(results[0]);
+  })
+});
+
+
+// NEW SERVICE JOB
+app.post('/api/servicejobs', (req, res) => {
+  const { error } = validateServiceJob(req.body);
+  if(error) return res.status(400).send(error.details[0].message);
+
+  const data = {
+    JobTitle:         req.body.JobTitle,
+    Description:      req.body.Description,
+    BriefDescription: req.body.BriefDescription,
+    Category:         req.body.Category,
+    CustomerID:       req.body.CustomerID,
+    BusinessID:       req.body.BusinessID,
+    QuotationID:      req.body.QuotationID,
+    PostedDate:       new Date,
+    DueDate:          req.body.DueDate,
+    CompletedDate:    req.body.CompletedDate
+  }
+  
+  const query = "INSERT INTO SERVICEJOB (JobTitle, Description, BriefDescription, Category, " 
+              + "CustomerID, BusinessID, QuotationID, PostedDate, DueDate, CompletedDate) "
+              + "values (?,?,?,?,?,?,?,?,?,?)";
+  pool.query(query, Object.values(data), (error) =>{
+    if (error) res.json({status: "Failure!", reason: error.code});
+    else res.json({status: "success", data: data});
+  })
+});
+
+
+
+// DELETE specific SERVICE JOB by ID
+app.delete('/api/servicejobs/:id', (req, res) => {
+  //first check to see if there is a user with that ID
+  let query = "select * from SERVICEJOB WHERE id = ?";
+
+  pool.query(query, [req.params.id], (error, results) =>{
+    //if no user with that id could be found
+    if (!results[0]) res.json({status: "Could not find a service job with that ID to delete"});
+    else {
+      query = "DELETE from SERVICEJOB WHERE id = ?";
+      pool.query(query, [req.params.id], (error, results) =>{
+        if (error) res.json({status: "error", reason: error.code});
+        else res.json({status: "success", reason: results});
+      })
+    }
+  })
+});
+
+// UPDATE SERVICE JOB
+app.put('/api/servicejobs/:id', (req, res) => {
+  //first check to see if there is a user with that ID
+  let query = "select * from SERVICEJOB WHERE id = ?";
+
+  //return with error if data does not pass validation
+  const { error } = validateServiceJob(req.body);
+  if(error) return res.status(400).send(error.details[0].message);
+
+  const data = {
+    JobTitle:         req.body.JobTitle,
+    Description:      req.body.Description,
+    BriefDescription: req.body.BriefDescription,
+    Category:         req.body.Category,
+    CustomerID:       req.body.CustomerID,
+    BusinessID:       req.body.BusinessID,
+    QuotationID:      req.body.QuotationID,
+    PostedDate:       new Date,
+    DueDate:          req.body.DueDate,
+    CompletedDate:    req.body.CompletedDate,
+    ID:               req.params.id
+  }
+  
+  pool.query(query, [req.params.id], (error, results) =>{
+    //if no user with that id could be found
+    if (!results[0]) res.json({status: "Could not find a service job with that ID to update"});
+    else {
+      //update the user that was found
+      query = "UPDATE servicejob SET JobTitle = ?, Description = ?, BriefDescription = ?, "
+            + "Category = ?, CustomerID = ?, BusinessID = ?, QuotationID = ?, PostedDate = ?, "
+            + "DueDate = ? WHERE id = ?";
+      pool.query(query, Object.values(data), (error) =>{
+        if (error) res.json({status: "Failure!", reason: error.code});
+        else res.json({status: "success", data: data});
+      })
+    }
+  })
+});
+
+//VALIDATION
+function validateServiceJob(servicejob){
+  const schema = Joi.object({
+    JobTitle:         Joi.string().max(40).min(6).required(),
+    Description:      Joi.string().max(1000).min(60).required(),
+    BriefDescription: Joi.string().max(255).min(25).required(),
+    Category:         Joi.number(),
+    CustomerID:       Joi.number(),
+    BusinessID:       Joi.number(),
+    QuotationID:      Joi.number(),
+    PostedDate:       Joi.date(),
+    DueDate:          Joi.date(),
+    CompletedDate:    Joi.date(),
+  });
+  
+  return schema.validate(servicejob);
 }
 
 
